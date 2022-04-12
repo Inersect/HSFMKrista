ecf1d097a057c1e5bc931c9d7c1b7886fb8fe588
Inersect <76960385+Inersect@users.noreply.github.com>
2022-04-12 21:42:37 +0200

    Kaart hatsjoe

--- Javascript/Maplibre.js
+++ Javascript/Maplibre.js
@@ -1,6 +1,6 @@
-var map = new maplibregl.Map({
+var mapLibre = new maplibregl.Map({
     container: 'mapLibre',
     style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
-    center: [-102.552788, 23.634501], // starting position [lng, lat]
-    zoom: 4 // starting zoom
-    });
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
--- Javascript/main.js
+++ Javascript/main.js
@@ -1,4 +1,4 @@
-var Leafletmap = L.map('map').setView([51.505, -0.09], 13);
+var Leafletmap = L.map('mapLeaflet').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -9,35 +9,35 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoiaW5lcnNlY3QiLCJhIjoiY2wwc2p0YXJnMDBkZTNjbW04ZjZnaGhubSJ9.Bbr6AwoqVPdthi0AbRTz9A'
     }).addTo(Leafletmap);
 
-const urlGeoserverWMS = "http://localhost:8080/geoserver/cite/wms?service=WMS&version=1.1.0&request=GetMap&layers=cite:provincie&styles=&bbox=634.5732789819012,306594.5543000576,284300.0254094796,636981.7698870846&width=659&height=768&srs=EPSG:28992&format=application/openlayers"
+// const urlGeoserverWMS = "http://localhost:8080/geoserver/cite/wms?service=WMS&version=1.1.0&request=GetMap&layers=cite:provincie&styles=&bbox=634.5732789819012,306594.5543000576,284300.0254094796,636981.7698870846&width=659&height=768&srs=EPSG:28992&format=application/openlayers"
 
 
 //arrays
 
-const geoJsonLayerNieuw= L.geoJSON().addTo(Leafletmap);
+// const geoJsonLayerNieuw= L.geoJSON().addTo(Leafletmap);
 
-const arrayvanplaatsnamen = ['Amsterdam', 'Deventer', 'Almere', 'Haren', 'Utrecht'];
+// const arrayvanplaatsnamen = ['Amsterdam', 'Deventer', 'Almere', 'Haren', 'Utrecht'];
 
-for (let index = 0; index < arrayvanplaatsnamen.length; index++) {
-    const woonplaats = arrayvanplaatsnamen [index];
-    console.log(woonplaats)
+// for (let index = 0; index < arrayvanplaatsnamen.length; index++) {
+//     const woonplaats = arrayvanplaatsnamen [index];
+//     console.log(woonplaats)
 
-    const node = document.createElement("button");
-    node.className = "button";
-    const textnode = document.createTextNode(woonplaats);
-    node.appendChild(textnode);
+//     const node = document.createElement("button");
+//     node.className = "button";
+//     const textnode = document.createTextNode(woonplaats);
+//     node.appendChild(textnode);
 
-    document.getElementById("header").appendChild(node);
+//     document.getElementById("header").appendChild(node);
 
-    node.addEventListener('click', function(){
-        console.log( node.id)
-    })
+//     node.addEventListener('click', function(){
+//         console.log( node.id)
+//     })
 
-    fetch('https://geodata.nationaalgeoregister.nl/locatieserver/free?bq=type:woonplaats&q='+ node.id)
-    .then(response => response.json())
-    .then (data => {
-        console.log(data.response.docs[0].id)
-    })
+//     fetch('https://geodata.nationaalgeoregister.nl/locatieserver/free?bq=type:woonplaats&q='+ node.id)
+//     .then(response => response.json())
+//     .then (data => {
+//         console.log(data.response.docs[0].id)
+//     })
 
-}
+// }
 
